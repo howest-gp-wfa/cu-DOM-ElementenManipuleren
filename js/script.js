7d00fa7b5cad357a13d2fc179bb1d8622ef21a35
Mileto <mileto.di.marco@howest.be>
2020-02-13 11:17:45 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+//declare global vars here
+ 
+ var allDivs;
+ var divByGetElementById;
+ var divByQuerySelector
+ 
+window.addEventListener('load',Initialize);
+
+function Initialize()
+{
+   
+   /*single HTMl element selection*/
+      //get a single element with getElementById
+      
+      //change the color
+      
+      //get a single element with querySelector
+      
+      //change color back to pink
+      
+   /*multiple HTML element selection*/
+      //get all the divs using name attribute
+      
+      //change content of first Div
+      
+      //get all the divs using tag name
+      
+      //change color of last div
+      
+      //get all the elements by classname
+      
+      //change content and color of last div
+      
+      //get all divs using queryselectorall
+      
+      //change content of second div
+      
+      //get all the divs using queryselectorAll and class name
+      
+      //change the color of third element
+      
+}
+
+
+
